913b1623636d25cc3f635fb1069fb2597aec0d6e
nitagonita <nitagonita@gmail.com>
2025-12-08 21:29:56 -0500

    Fix router + base for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
-import { Routes, Route } from 'react-router-dom'
-import Layout from './components/Layout.jsx'
-import Home from './pages/Home.jsx'
-import Search from './pages/Search.jsx'
-import Results from './pages/Results.jsx'
-import MovieDetail from './pages/MovieDetail.jsx'
-import Favourites from './pages/Favourites.jsx'
-import NotFound from './pages/NotFound.jsx'
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout.jsx";
+import Home from "./pages/Home.jsx";
+import Search from "./pages/Search.jsx";
+import Results from "./pages/Results.jsx";
+import MovieDetail from "./pages/MovieDetail.jsx";
+import Favourites from "./pages/Favourites.jsx";
+import NotFound from "./pages/NotFound.jsx";
 
-export default function App(){
+export default function App() {
   return (
     <Routes>
-      <Route element={<Layout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/results" element={<Results />} />
-        <Route path="/movie/:imdbID" element={<MovieDetail />} />
-        <Route path="/favourites" element={<Favourites />} />
-        <Route path="*" element={<NotFound />} />
+      <Route path="/" element={<Layout />}>
+        {/* halaman utama */}
+        <Route index element={<Home />} />
+
+        {/* halaman lain */}
+        <Route path="search" element={<Search />} />
+        <Route path="results" element={<Results />} />
+        <Route path="movie/:imdbID" element={<MovieDetail />} />
+        <Route path="favourites" element={<Favourites />} />
       </Route>
+
+      {/* kalau semua route di atas tidak cocok */}
+      <Route path="*" element={<NotFound />} />
     </Routes>
-  )
+  );
 }
--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/movie-explorer/", // penting untuk GitHub Pages
-  build: {
-    outDir: "docs", // hasil build masuk ke folder docs
-  },
+  base: "/movie-explorer/", // penting utk GitHub Pages
 });
