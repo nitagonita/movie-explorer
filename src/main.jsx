72d79ad9d12341cf2c4592719c8c4fadbddb88d9
nitagonita <nitagonita@gmail.com>
2025-12-25 10:20:14 -0500

    main.jsx2

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import ScrollToTop from "./components/ScrollToTop.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/movie-explorer">
+    <BrowserRouter>
       <ScrollToTop />
       <App />
     </BrowserRouter>
